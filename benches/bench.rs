0dc9b3e5a33bd9ff7a6cfa19c09a819fd18c28ac
Demur Rumed <gunkmute@gmail.com>
2017-03-26 02:44:57 +0000

    Replace time with std::time

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,10 +1,10 @@
 #![feature(test)]
 extern crate test;
-extern crate time;
 extern crate ocl;
 
 extern crate geogrid;
 
+use std::time::{Duration, Instant};
 use geogrid::util::*;
 use ocl::builders::DeviceSpecifier;
 
@@ -13,7 +13,6 @@ use test::{Bencher, black_box};
 
 const M: usize = 7000;
 const N: usize = M;
-const NS_PER_S: f64 = 1e6;
 fn mock_shape(sz: usize) -> Vec<Vec<bool>> {
     let mut sp = vec![vec![false; sz]; sz];
     // Set some of them to true
@@ -25,16 +24,19 @@ fn mock_shape(sz: usize) -> Vec<Vec<bool>> {
     sp
 }
 
+fn dur_as_ms(dur: Duration) -> f64 {
+    dur.as_secs() as f64 * 1000.0 + dur.subsec_nanos() as f64 / 1000_000.0
+}
+
 // test match of 60x60 shape on 7000x7000 maps.
 #[bench]
 fn bench_singlecore(_: &mut Bencher) {
     println!();
     let sp = mock_shape(60);
     let dt = vec![2; M * N];
-    let start = time::precise_time_ns();
+    let start = Instant::now();
     black_box(match_shape_slow(&dt, (M, N), &sp));
-    let end = time::precise_time_ns();
-    println!("Time elapsed: {:.2} ms", (end - start) as f64 / NS_PER_S);
+    println!("Time elapsed: {:.2} ms", dur_as_ms(start.elapsed()));
 }
 
 #[bench]
@@ -42,10 +44,9 @@ fn bench_multicore(_: &mut Bencher) {
     println!();
     let sp = mock_shape(60);
     let dt = vec![2; M * N];
-    let start = time::precise_time_ns();
+    let start = Instant::now();
     black_box(match_shape(&dt, (M, N), &sp));
-    let end = time::precise_time_ns();
-    println!("Time elapsed: {:.2} ms", (end - start) as f64 / NS_PER_S);
+    println!("Time elapsed: {:.2} ms", dur_as_ms(start.elapsed()));
 }
 
 #[bench]
@@ -55,11 +56,10 @@ fn bench_ocl(_: &mut Bencher) {
     let dt = vec![2; M * N];
     if let Ok(all_devices) = (DeviceSpecifier::All).to_device_list(None) {
         for device in all_devices {
-            let start = time::precise_time_ns();
+            let start = Instant::now();
             println!("{}", device.name());
             black_box(match_shape_ocl(&dt, (M, N), &sp, device));
-            let end = time::precise_time_ns();
-            println!("Time elapsed: {:.2} ms", (end - start) as f64 / NS_PER_S);
+            println!("Time elapsed: {:.2} ms", dur_as_ms(start.elapsed()));
         }
     }
 }
